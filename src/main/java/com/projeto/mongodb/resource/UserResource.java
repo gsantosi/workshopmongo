3e40874fe61c31b883bcdbdae614c101d5338e51
Guilherme Santos <guilheermeinnocencio15@gmail.com>
2019-05-22 10:12:47 -0300

    Metodo Delete criado

--- src/main/java/com/projeto/mongodb/resource/UserResource.java
+++ src/main/java/com/projeto/mongodb/resource/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,12 +41,19 @@ public class UserResource {
 	@PostMapping(value="/insert")
 	public ResponseEntity<Void> insert(@RequestBody User obj) {
      service.insert(obj);
-	return null;
+	return ResponseEntity.ok().build();
 	}
 
 	@DeleteMapping(value="/delete/{id}")
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
+		return ResponseEntity.ok().build();
+		
+	}
+	
+	@PutMapping(value="/update")
+	public ResponseEntity<Void> update(@RequestBody User obj){
+		service.update(obj);
 		return ResponseEntity.ok().build();
 		
 	}
